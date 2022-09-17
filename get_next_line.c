ccfef35721bd07594dbb7b39c779a3ba1f93a59f
carl0sfelipe <carlos.felipe@hotmail.com.br>
2022-09-17 02:22:08 -0300

    try

--- get_next_line.c
+++ get_next_line.c
@@ -12,24 +12,78 @@
 
 #include "get_next_line.h"
 
+char	*get_remaining(char *static_str)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (static_str[i] && static_str[i] != '\n')
+		i++;
+	if (!static_str[i])
+	{
+		free(static_str);
+		return (NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(static_str) - i + 1));
+	if (!str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (static_str[i])
+		str[j++] = static_str[i++];
+	str[j] = '\0';
+	free(static_str);
+	return (str);
+}
+
+char	*ft_get_line(char *static_str)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!static_str[i])
+		return (NULL);
+	while (static_str[i] && static_str[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (static_str[i] && static_str[i] != '\n')
+	{
+		str[i] = static_str[i];
+		i++;
+	}
+	if (static_str[i] == '\n')
+	{
+		str[i] = static_str[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
 char	*read_until_bar_n(int fd, char *static_str)
 {
 	char	*buff;
-	int		red_bytes;
+	int		bytes_red;
 
 	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
-	red_bytes = 1;
-	while (!ft_strchr(static_str, '\n') && red_bytes > 0)
+	bytes_red = 1;
+	while (!ft_strchr(static_str, '\n') && bytes_red > 0)
 	{
-		red_bytes = read(fd, buff, BUFFER_SIZE);
-		if (red_bytes == -1)
+		bytes_red = read(fd, buff, BUFFER_SIZE);
+		if (bytes_red == -1)
 		{
 			free(buff);
 			return (NULL);
 		}
-		buff[red_bytes] = '\0';
+		buff[bytes_red] = '\0';
 		static_str = ft_strjoin(static_str, buff);
 	}
 	free(buff);
--- get_next_line.h
+++ get_next_line.h
@@ -14,9 +14,8 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <fcntl.h>
+# include<unistd.h>
+# include<fcntl.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1000
